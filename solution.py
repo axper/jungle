5c9b6c084b71477491b7e579b51facc2fc3f40a3
Babken Vardanyan <babken@lazydevelo.com>
2018-04-19 20:28:05 +0400

    Added logging, move database url to separate config file

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import config
 from models import Base
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+logging.basicConfig(level=logging.DEBUG)
+
+engine = create_engine(config.DATABASE_URL, echo=False)
+logging.info('Created engine: {}'.format(engine))
 Session = sessionmaker(bind=engine)
 session = Session()
+logging.info('Created session: {}'.format(session))
 Base.metadata.create_all(engine)
+logging.info('Created database schema.')
